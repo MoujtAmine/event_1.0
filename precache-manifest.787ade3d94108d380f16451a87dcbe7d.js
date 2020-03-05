704a75d260322eb28bcabce4e84947db33b3a043
moujtahid amine <moujtahid.amine95@gmil.com>
2020-03-05 07:28:29 +0100

    Updates

---
+++ precache-manifest.787ade3d94108d380f16451a87dcbe7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "839b573c880632a35cd3b57fa3aceb28",
+    "url": "/event_1.0/index.html"
+  },
+  {
+    "revision": "2e2999107bdc3484adff",
+    "url": "/event_1.0/static/css/2.2a1fcfe6.chunk.css"
+  },
+  {
+    "revision": "c246974a669494d0719b",
+    "url": "/event_1.0/static/css/main.98165009.chunk.css"
+  },
+  {
+    "revision": "2e2999107bdc3484adff",
+    "url": "/event_1.0/static/js/2.17c33d37.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/event_1.0/static/js/2.17c33d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c246974a669494d0719b",
+    "url": "/event_1.0/static/js/main.1a0649ea.chunk.js"
+  },
+  {
+    "revision": "08dee5be605e809ba393",
+    "url": "/event_1.0/static/js/runtime-main.edc8c343.js"
+  }
+]);
